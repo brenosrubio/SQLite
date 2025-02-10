e9b30e6acfaa960638734adbf9b82915b95e01fa
Breno Silva Rubio <108412937+brenosrubio@users.noreply.github.com>
2025-02-10 17:46:27 -0300

    Create 03 - Resolução questão 6.sql

---
+++ Atividade/03 - Resolução questão 6.sql
@@ -0,0 +1,5 @@
+-- Atualize o salário dos funcionários do departamento de "TI" para 7500 na tabela funcionarios.
+
+UPDATE funcionarios SET salario = 7500 WHERE departamento = 'TI';
+
+SELECT * FROM funcionarios WHERE departamento = 'TI';
