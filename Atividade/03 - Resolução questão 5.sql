30d6c87eff5685897edbf789c828ff79bb638b64
Breno Silva Rubio <108412937+brenosrubio@users.noreply.github.com>
2025-02-10 17:45:25 -0300

    Update 03 - Resolução questão 5.sql

--- Atividade/03 - Resolução questão 5.sql
+++ Atividade/03 - Resolução questão 5.sql
@@ -1,3 +1,3 @@
--- 5) Na tabela funcionarios, selecione todos os departamentos distintos.
+-- Na tabela funcionarios, selecione todos os departamentos distintos.
 
 SELECT DISTINCT departamento FROM funcionarios;
